71b69b239ffdd353106efeb8668f9307ad85fa7a
Jonathan Bohorquez <jonathan.bohorquez@tsicol.com>
2025-08-04 03:18:03 -0500

    Pruebas unitarias e integracion inventory

--- src/main/java/com/example/inventory/InventoryApplication.java
+++ src/main/java/com/example/inventory/InventoryApplication.java
@@ -4,9 +4,11 @@ import com.example.inventory.config.ApiKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApiKeyProperties.class)
+@EnableRetry
 public class InventoryApplication {
 
 	public static void main(String[] args) {
